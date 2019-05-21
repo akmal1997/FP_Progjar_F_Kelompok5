6c8afc3a13dc2c10346a913290a9928adc131294
Mujahid97 <33047680+Mujahid97@users.noreply.github.com>
2019-05-21 18:48:17 +0700

    Yaaa begitulah

--- chat-cli.py
+++ chat-cli.py
@@ -5,80 +5,81 @@ import json
 TARGET_IP = "127.0.0.1"
 TARGET_PORT = 8889
 
-
+	
 class ChatClient:
-    def __init__(self):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_address = (TARGET_IP,TARGET_PORT)
-        self.sock.connect(self.server_address)
-        self.tokenid=""
-        self.username=""
-    def proses(self,cmdline):
-	j=cmdline.split(" ")
-	try:
-	    command=j[0].strip()
-	    if (command=='auth'):
-		username=j[1].strip()
-		password=j[2].strip()
-		return self.login(username,password)
-	    elif (command=='send'):
-		usernameto = j[1].strip()
-                message=""
-                for w in j[2:]:
-                   message="{} {}" . format(message,w)
-		return self.sendmessage(usernameto,message)
-            elif (command=='logout'):
+	def __init__(self):
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.server_address = (TARGET_IP, TARGET_PORT)
+		self.sock.connect(self.server_address)
+		self.tokenid = ""
+		self.username=""
+	def proses(self, cmdline):
+		j = cmdline.split(" ")
+		try:
+			command = j[0].strip()
+			if (command == 'auth'):
+				username = j[1].strip()
+				password = j[2].strip()
+				return self.login(username, password)
+			
+			elif (command == 'send'):
+				usernameto = j[1].strip()
+				message = ""
+				for w in j[2:]:
+					message = "{} {}" . format(message, w)
+				return self.sendmessage(usernameto, message)
+			
+			elif (command == 'inbox'):
+				return self.inbox()
+			
+			elif (command == 'logout'):
 				return self.logout()
-            elif (command=='inbox'):
-                return self.inbox()
-            elif(command=='create_group'):
-                group = j[1].strip()
-                return self.create_group(group)
-            elif(command=='join_group'):
-                group = j[1].strip()
-                return self.join_group(group)
-            elif(command=='send_group'):
-                group = j[1].strip()
-                message=""
-                for w in j[2:]:
-                    message="{} {}" . format(message, w)
-                return self.sendmessage_group(group, message)
-            elif(command=='inbox_group'):
-                group = j[1].strip()
-                return self.inbox_group(group)
-            elif(command=='leave_group'):
-                group = j[1].strip()
-                return self.leave_group(group)
-	    else:
-		return "*Maaf, command tidak benar"
-	except IndexError:
-	    return "-Maaf, command tidak benar"
+			
+			elif (command == 'create_group'):
+				groupname = j[1]
+				return self.create_group(groupname)
+			
+			elif (command == 'join_group'):
+				groupname = j[1]
+				return self.join_group(groupname)
+			
+			elif (command == 'send_group'):
+				groupnameto = [1]
+				groupmessage=""
+				for w in j[2:]:
+					groupmessage="{} {}".format(groupmessage,w)
+				return self.send_group(groupnameto, groupmessage)
+			else:
+				return "*Command not found, type `menu` to see all commands"
+		except IndexError:
+			return "*Command format wrong, type `menu` to see all commands"
 
-    def sendstring(self,string):
-        try:
-            self.sock.sendall(string)
-            receivemsg = ""
-            while True:
-                data = self.sock.recv(10)
-                if (data):
-                    receivemsg = "{}{}" . format(receivemsg,data)
-                    if receivemsg[-4:]=="\r\n\r\n":
-                        return json.loads(receivemsg)
-        except:
-            self.sock.close()
+	def sendstring(self, string):
+		try:
+			self.sock.sendall(string)
+			receivemsg = ""
+			while True:
+				data = self.sock.recv(10)
+				if (data):
+					receivemsg = "{}{}" . format(receivemsg, data)
+					if receivemsg[-4:] == "\r\n\r\n":
+						return json.loads(receivemsg)
+		except:
+			self.sock.close()
 
 	def logout(self):
 		if (self.tokenid == ""):
-			return "Sudah log out"
+			return "Already logged out"
 		string = "logout {} \r\n" . format(self.tokenid)
 		result = self.sendstring(string)
 		if result['status'] == 'OK':
 			self.tokenid = ""
-			return "Log out berhasil"
+			self.username = ""
+			return "Log out successful"
 		else:
-			return "Log out gagal"
+			return "Unexpected Error(?)"
 
-    def login(self, username, password):
+	def login(self, username, password):
 		if(self.tokenid != ""):
 			return "You already logged in"
 		string = "auth {} {} \r\n" . format(username, password)
@@ -89,30 +90,63 @@ class ChatClient:
 			return "username {} logged in, token {} " .format(username, self.tokenid)
 		else:
 			return "Error, {}" . format(result['message'])
-    
-    def sendmessage(self,usernameto="xxx",message="xxx"):
-        if (self.tokenid==""):
-            return "Error, not authorized"
-        string="send {} {} {} \r\n" . format(self.tokenid,usernameto,message)
-        result = self.sendstring(string)
-        if result['status']=='OK':
-            return "message sent to {}" . format(usernameto)
-        else:
-            return "Error, {}" . format(result['message'])
-    def inbox(self):
-        if (self.tokenid==""):
-            return "Error, not authorized"
-        string="inbox {} \r\n" . format(self.tokenid)
-        result = self.sendstring(string)
-        if result['status']=='OK':
-            return "{}" . format(json.dumps(result['messages']))
-        else:
-            return "Error, {}" . format(result['message'])
 
+	def sendmessage(self, usernameto="xxx", message="xxx"):
+		if (self.tokenid == ""):
+			return "Error, not authorized"
+		string = "send {} {} {} \r\n" . format(
+			self.tokenid, usernameto, message)
+		result = self.sendstring(string)
+		if result['status'] == 'OK':
+			return "message sent to {}" . format(usernameto)
+		else:
+			return "Error, {}" . format(result['message'])
 
+	def inbox(self):
+		if (self.tokenid == ""):
+			return "Error, not authorized"
+		string = "inbox {} \r\n" . format(self.tokenid)
+		result = self.sendstring(string)
+		if result['status'] == 'OK':
+			return "{}" . format(json.dumps(result['messages']))
+		else:
+			return "Error, {}" . format(result['message'])
 
-if __name__=="__main__":
-    cc = ChatClient()
-    while True:
-        cmdline = raw_input("Command {}:" . format(cc.tokenid))
-        print cc.proses(cmdline)
+	def create_group(self, groupname):
+		if (self.tokenid==""):
+			return "Error, non authorized"
+		string = "create_group {} {} \r\n" . format(self.tokenid, groupname)
+		result = self.sendstring(string)
+
+		if result['status'] == 'OK':
+			return "{}" . format(result['message'])
+		else:
+			return "Error, {}" .format(json.dump(result['message']))
+
+	def join_group(self, groupname):
+		if (self.tokenid==""):
+			return "Error, not authorized"
+		string ="join_group {} {} \r\n" .format(self.tokenid, groupname)
+		result = self.sendstring(string)
+
+		if (result['status']== 'OK'):
+			return "{}" .format(result['message'])
+		else:
+			return "Error, {}".format(json.dumps(result['message']))
+
+	def send_group(self, groupnameto, groupmessage):
+    		if(self.tokenid==""):
+    				return "Error, not authorized"
+				string ="send_group {} {} {}\r\n" .format(self.tokenid,groupnameto)
+				result =self.sendstring(string)
+
+				if result['status']=='OK':
+    					return "{}".format(result['message'])
+				else:
+    					return "Error, {}".format(json.dumps(result['message']))
+
+if __name__ == "__main__":
+	cc = ChatClient()
+	while True:
+		cmdline = raw_input("Command {}:" . format(cc.tokenid))
+		print cc.proses(cmdline)
--- chat.py
+++ chat.py
@@ -4,116 +4,189 @@ import json
 import uuid
 from Queue import *
 
+
 class Chat:
 	def __init__(self):
 		self.online_users = []
-		self.sessions={}
+		self.sessions = {}
 		self.users = {}
-		self.users['messi']={ 'nama': 'Lionel Messi', 'negara': 'Argentina', 'password': 'surabaya', 'incoming' : {}, 'outgoing': {}}
-		self.users['henderson']={ 'nama': 'Jordan Henderson', 'negara': 'Inggris', 'password': 'surabaya', 'incoming': {}, 'outgoing': {}}
-		self.users['lineker']={ 'nama': 'Gary Lineker', 'negara': 'Inggris', 'password': 'surabaya','incoming': {}, 'outgoing':{}}
-	def proses(self,data):
-		j=data.split(" ")
+		self.groups ={}
+		self.users['messi'] = {'nama': 'Lionel Messi', 'negara': 'Argentina',
+							   'password': 'surabaya', 'incoming': {}, 'outgoing': {}}
+		self.users['henderson'] = {'nama': 'Jordan Henderson', 'negara': 'Inggris',
+								   'password': 'surabaya', 'incoming': {}, 'outgoing': {}}
+		self.users['lineker'] = {'nama': 'Gary Lineker', 'negara': 'Inggris',
+								 'password': 'surabaya', 'incoming': {}, 'outgoing': {}}
+
+	def proses(self, data):
+		j = data.strip().split(" ")
 		try:
-			command=j[0].strip()
-			if (command=='auth'):
-				username=j[1].strip()
-				password=j[2].strip()
-                                print "auth {}" . format(username)
-				return self.autentikasi_user(username,password)
-			elif (command=='send'):
-				sessionid = j[1].strip()
-				usernameto = j[2].strip()
-                                message=""
-                                for w in j[3:]:
-                                    message="{} {}" . format(message,w)
+			command = j[0]
+			if (command == 'auth'):
+				username = j[1]
+				password = j[2]
+				print "{} logging in" . format(username)
+				return self.autentikasi_user(username, password)
+			elif (command == 'send'):
+				sessionid = j[1]
+				usernameto = j[2]
+				message = ""
+				for w in j[3:]:
+					message = "{} {}" . format(message, w)
 				usernamefrom = self.sessions[sessionid]['username']
-                                print "send message from {} to {}" . format(usernamefrom,usernameto)
-				return self.send_message(sessionid,usernamefrom,usernameto,message)
-                        elif (command=='inbox'):
-                                sessionid = j[1].strip()
-                                username = self.sessions[sessionid]['username']
-                                print "inbox {}" . format(sessionid)
-                                return self.get_inbox(username)
+				print "sending message from {} to {}" . format(
+					usernamefrom, usernameto)
+				return self.send_message(sessionid, usernamefrom, usernameto, message)
+			elif (command == 'inbox'):
+				sessionid = j[1]
+				username = self.sessions[sessionid]['username']
+				print "inbox {}" . format(sessionid)
+				return self.get_inbox(username)
+			elif (command == 'logout'):
+				sessionid = j[1]
+				username = self.sessions[sessionid]['username']
+				print "{} logging out" . format(username)
+				return self.logout_user(sessionid, username)
+			elif (command=='create_group'):
+				sessionid = j[1]
+				groupname = j[2]
+				return self.join_group(sessionid,groupname)
+			elif (command =='join_group'):
+				sessionid = j[1]
+				groupnameto = j[2]
+				sender = self.sessions[sessionid]['username']
+				groupmessage=""								
+				for w in j[3]:
+					groupmessage = "{} {}".format(groupmessage,w)
+				print groupmessage
+				return self.send_group(sender, groupnameto, groupmessage)
 			else:
 				return {'status': 'ERROR', 'message': '**Protocol Tidak Benar'}
 		except IndexError:
 			return {'status': 'ERROR', 'message': '--Protocol Tidak Benar'}
-	def autentikasi_user(self,username,password):
+
+	def autentikasi_user(self, username, password):
 		if (username not in self.users):
-			return { 'status': 'ERROR', 'message': 'User Tidak Ada' }
- 		if (self.users[username]['password']!= password):
-			return { 'status': 'ERROR', 'message': 'Password Salah' }
-		tokenid = str(uuid.uuid4()) 
-		self.sessions[tokenid]={ 'username': username, 'userdetail':self.users[username]}
-		return { 'status': 'OK', 'tokenid': tokenid }
-	def get_user(self,username):
+			return {'status': 'ERROR', 'message': 'User Tidak Ada'}		
+		if (self.users[username]['password'] != password):
+			return {'status': 'ERROR', 'message': 'Password Salah'}
+		if (username in self.online_users):
+			return {'status': 'ERROR', 'message': 'User already logged in'}
+		tokenid = str(uuid.uuid4())
+		self.sessions[tokenid] = {
+			'username': username, 'userdetail': self.users[username]}
+		self.online_users.append(username)
+		print "{} logged in successfully" . format(username)
+		print "online users: {}" . format(self.online_users)
+		return {'status': 'OK', 'tokenid': tokenid}
+
+	def logout_user(self, sessionid, username):
+		if (sessionid in self.sessions):
+			del self.sessions[sessionid]
+			print "{} logged out successfully" . format(username)
+			print "online users: {}" . format(self.online_users)
+		return {'status' : 'OK'}
+
+	def get_user(self, username):
 		if (username not in self.users):
 			return False
 		return self.users[username]
-	def send_message(self,sessionid,username_from,username_dest,message):
+
+	def send_message(self, sessionid, username_from, username_dest, message):
 		if (sessionid not in self.sessions):
 			return {'status': 'ERROR', 'message': 'Session Tidak Ditemukan'}
 		s_fr = self.get_user(username_from)
 		s_to = self.get_user(username_dest)
-		
-		if (s_fr==False or s_to==False):
+
+		if (s_fr == False or s_to == False):
 			return {'status': 'ERROR', 'message': 'User Tidak Ditemukan'}
 
-		message = { 'msg_from': s_fr['nama'], 'msg_to': s_to['nama'], 'msg': message }
+		message = {'Type': 'Personal', 'msg_from': s_fr['nama'], 'msg_to': s_to['nama'], 'msg': message }
 		outqueue_sender = s_fr['outgoing']
 		inqueue_receiver = s_to['incoming']
-		try:	
+		try:
 			outqueue_sender[username_from].put(message)
 		except KeyError:
-			outqueue_sender[username_from]=Queue()
+			outqueue_sender[username_from] = Queue()
 			outqueue_sender[username_from].put(message)
 		try:
 			inqueue_receiver[username_from].put(message)
 		except KeyError:
-			inqueue_receiver[username_from]=Queue()
+			inqueue_receiver[username_from] = Queue()
 			inqueue_receiver[username_from].put(message)
 		return {'status': 'OK', 'message': 'Message Sent'}
 
-	def get_inbox(self,username):
+	def get_inbox(self, username):
 		s_fr = self.get_user(username)
 		incoming = s_fr['incoming']
-		msgs={}
+		msgs = {}
 		for users in incoming:
-			msgs[users]=[]
+			msgs[users] = []
 			while not incoming[users].empty():
 				msgs[users].append(s_fr['incoming'][users].get_nowait())
-			
+
 		return {'status': 'OK', 'messages': msgs}
 
+	def logout(self,sessionsid):
+		if(sessionsid in self.sessions):
+    			del self.sessions[sessionsid]
+		return {'status':'OK'}
 
-if __name__=="__main__":
+	def create_group(self, sessionid, groupname):
+		if(groupname in self.groups):
+			return {'status': 'ERROR', 'message': 'group sudah ada'}
+		
+		admin = self.sessions[sessionid]['username']
+		self.groups[groupname] = {'admin':admin, 'user':[]}
+		self.groups[groupname]['users'].append(admin)
+		return {'status':'OK', 'message': '{} created'.format(groupname)}
+
+	def join_group(self, sessionid, groupname):
+    		if (groupname not in self.groups):
+			return { 'status': 'ERROR', 'message': 'Group tidak ada' }
+
+		member = self.sessions[sessionid]['username']
+		self.groups[groupname]['users'].append(member)
+		print self.groups[groupname]['users']
+		return {'status':'OK', 'message': '{} joined {}'.format(member, groupname)}
+
+	def sendto_group(self, username, groupnameto, groupmessage):
+		if (groupnameto not in self.groups):
+			return { 'status': 'ERROR', 'message': 'Group tujuan tidak ada' }
+
+		sender = self.get_user(username)
+		if (sender==False):
+			return {'status': 'ERROR', 'message': 'User Tidak Ditemukan'}
+
+		print groupmessage
+		for tousername in self.groups[groupnameto]['users']:
+			reciever = self.get_user(tousername)
+			message = {'Type': 'Group', 'msg_from': sender['nama'], 'msg_to': togroupname, 'msg': groupmessage }
+			outqueue_sender = sender['outgoing']
+			inqueue_receiver = reciever['incoming']
+			try:	
+				outqueue_sender[username].put(message)
+			except KeyError:
+				outqueue_sender[username]=Queue()
+				outqueue_sender[username].put(message)
+			try:
+				inqueue_receiver[username].put(message)
+			except KeyError:
+				inqueue_receiver[username]=Queue()
+
+		return {'status': 'OK', 'message': 'Message Sent'}	
+
+if __name__ == "__main__":
 	j = Chat()
-        sesi = j.proses("auth messi surabaya")
+	sesi = j.proses("auth messi surabaya")
 	print sesi
 	#sesi = j.autentikasi_user('messi','surabaya')
 	#print sesi
 	tokenid = sesi['tokenid']
-	print j.proses("send {} henderson hello gimana kabarnya son " . format(tokenid))
+	print j.proses(
+		"send {} henderson hello gimana kabarnya son " . format(tokenid))
 	#print j.send_message(tokenid,'messi','henderson','hello son')
 	#print j.send_message(tokenid,'henderson','messi','hello si')
 	#print j.send_message(tokenid,'lineker','messi','hello si dari lineker')
 
-
 	print j.get_inbox('messi')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
